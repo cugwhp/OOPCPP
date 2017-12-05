74017ca1e29e5332c8a28989b9ff3f368dc4f54e
cugwhp <wanghp119@gmail.com>
2017-12-05 22:16:13 +0800

    update

---
+++ 04.Code/Ch5/RSImageIO/RSImage.h
@@ -0,0 +1,44 @@
+#ifndef _RS_IMAGE_H_INC_
+#define _RS_IMAGE_H_INC_
+
+#ifndef NULL
+#define NULL 0
+#endif	//#ifndef NULL
+
+enum INTERLEAVE_TYPE {BSQ,BIP,BIL};
+
+class CRSImage
+{
+public:
+	typedef unsigned char	DataType;
+
+	CRSImage();
+	CRSImage(const CRSImage& img);
+	~CRSImage();
+
+	// Operations
+	bool	Open(const char* lpstrPath);
+	inline bool	IsOpen() const { return (NULL != m_pppData ? true : false);}
+	bool	Save(const char* lpstrPath);
+
+	inline int	GetBands() const {return m_nBands;}
+	inline int	GetLines() const {return m_nLines;}
+	inline int GetSamples() const {return m_nSamples;}
+	inline DataType*** GetDataBuffer() const {return m_pppData;}
+
+protected:
+	bool	ReadMetaData(const char* lpstrMetaFilePath);
+	bool	InitBuffer(void);
+	bool	ReadImgData(const char* lpstrImgFilePath);
+
+protected:
+	DataType***		m_pppData;
+	int				m_nBands;
+	int				m_nLines;
+	int				m_nSamples;
+	INTERLEAVE_TYPE m_eInterleave;
+	short       m_nDataType;
+private:
+};
+
+#endif	//#ifndef _RS_IMAGE_H_INC_
