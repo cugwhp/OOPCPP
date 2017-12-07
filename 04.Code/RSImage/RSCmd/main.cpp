098a845e1fa1531bd3dfa1de23547b98ee2bc303
cugwhp <wanghp119@gmail.com>
2017-12-07 23:03:47 +0800

    update

--- 04.Code/RSImage/RSCmd/main.cpp
+++ 04.Code/RSImage/RSCmd/main.cpp
@@ -2,10 +2,18 @@
 #include <string>
 #include <sstream>
 #include "RSImage.h"
+#include "Windows.h"	//windows
 
 using namespace std;
 
+// 前向声明，调用获取控制台窗口句柄
+// 因为是Win32全局函数，故使用extern "C"
+extern "C" HWND WINAPI GetConsoleWindow();
+
 void Usage();	//declaration forward
+
+// 前向声明，显示图像
+void DisplayImage(CRSImage* pRSImg);
 
 // 主函数
 int main()
@@ -32,8 +40,7 @@ int main()
 		case 'O':	// Open Image
 		case 'o':
 		    cout << "Input Image file path: ";
-//			cin >> strParam;
-			strParam = "d:\\test\\can_tmr";
+			cin >> strParam;
 			rsImg.Open(strParam.c_str());
 			break;
 
@@ -45,6 +52,14 @@ int main()
 		case 'C':	// Close Image
 		case 'c':
 			rsImg.Close();
+			break;
+
+		case 'D':
+		case 'd':
+			if (rsImg.IsOpen())
+				DisplayImage(&rsImg);
+			else
+				cerr << "Please Open image first." << endl;
 			break;
 
 		case 'S':	// Calculate Statistics
@@ -105,4 +120,49 @@ void Usage()
 	cout << "# Z – Zoom\t图像缩放，输入比例尺出" << endl;
 	cout << "# F - Filter\t输入滤波核，输出滤波后图像" << endl;
 	cout << "#################################################################" << endl;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// DisplayImage - 显示图像到控制台窗口									//
+// CRSImage* pRSImg - 图像数据指针										//
+// 返回值 - void														//
+//////////////////////////////////////////////////////////////////////////
+void DisplayImage(CRSImage* pRSImg)
+{
+	// 参数判断
+	if (pRSImg==NULL)
+		return;
+	
+	HWND	hwnd = NULL;//获得句柄
+	HDC		hdc = NULL;
+
+	// 窗口句柄为NULL，返回
+	hwnd = GetConsoleWindow();
+	if (hwnd == NULL)
+		return;
+
+	// 设备上下文返回NULL，程序返回
+	hdc = GetDC(hwnd);	
+	if (hdc == NULL)
+		return;
+
+	int		i, j;	
+	int		nBands = pRSImg->GetBands();		//波段
+	int		nSamples = pRSImg->GetSamples();	//列
+	int		nLines = pRSImg->GetLines();		//行
+	unsigned char***	pppData = pRSImg->GetDataBuffer();	//三维数组头指针
+	unsigned char**		ppRed = pppData[0];		//红色通道的波段
+	unsigned char**		ppGrn = nBands>2 ? pppData[1] : pppData[0];	//绿色通道的波段
+	unsigned char**		ppBlu = nBands>2 ? pppData[2] : pppData[0];	//蓝色通道的波段
+
+	// 遍历像素，显示图像
+	for (i=0; i<nLines; i++)
+	{
+		for (j=0; j<nSamples; j++)
+		{	//逐个像元输出
+			SetPixel(hdc,j, i, RGB(ppRed[i][j], ppGrn[i][j], ppBlu[i][j])); 	
+		}
+	}
+
+	ReleaseDC(hwnd,hdc);
 }
